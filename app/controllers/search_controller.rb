9dfcc69143d7c140786a3b26d1792088dfa31596
David Currin <dave@2young2die.com>
2009-02-03 12:35:23 +0100

    return search results according to page view permissions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,23 @@ class SearchController < ApplicationController
   def index
     if params[:query]
       @results = Article.paginate_search params[:query], :page => params[:page], :per_page => 20
-      @grouped_results = @results.group_by { |a| a.article_type }
+      @results.delete_if { |result| private?(result.page)}
+      @grouped_results = @results.group_by { |a| a.article_type } 
       @empty = true if @results.empty?
     else
       @empty = true
     end
   end
+  
+  private 
+  
+  def private?(page)
+    # Public Searches
+    if !logged_in? && !page.public?
+      return true
+    elsif logged_in? && !current_user.has_role?("#{page.viewable_by}")
+      return true
+    end
+  end
+  
 end
