3e13a1109c2fd999bd8687c7d10b842bdb4faca2
David Currin <dave@2young2die.com>
2009-02-10 13:43:07 +0100

    Add expiry of events pages to Page sweeper

--- app/sweepers/page_sweeper.rb
+++ app/sweepers/page_sweeper.rb
@@ -10,13 +10,20 @@ class PageSweeper < ActionController::Caching::Sweeper
   end
   
   def expire_cache
+    # We no longer cache the menu due to differing view
+    # permissions. This is obsolete
+    # TODO - cache the menu based on view permissions
     expire_fragment 'primary_menu'
     expire_fragment 'secondary_menu'
      
     # expire all pages
+    # TODO - use a regex for this rather than going to the database
     Page.find(:all, :select => "id, name").each do |p|
       expire_page(p)
     end 
+    
+    # expire all events pages with a regex
+    expire_fragment(%r{page-*}) 
   end
   
   private
