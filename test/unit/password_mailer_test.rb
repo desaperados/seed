6bf5ed55745bd0b61b70f4cd2ef8dffbfd132a99
Jaime Bellmyer <bellmyer@happy-laptop.local>
2009-12-11 09:48:32 -0600

    fixed PasswordMailer tests
    the default tests were still passing time to the create functions even though the functions had been updated to accept a password or user model.

--- test/unit/password_mailer_test.rb
+++ test/unit/password_mailer_test.rb
@@ -2,20 +2,25 @@ require 'test_helper'
 
 class PasswordMailerTest < ActionMailer::TestCase
   tests PasswordMailer
+  
   def test_forgot_password
-    @expected.subject = 'PasswordMailer#forgot_password'
-    @expected.body    = read_fixture('forgot_password')
-    @expected.date    = Time.now
-
-    assert_equal @expected.encoded, PasswordMailer.create_forgot_password(@expected.date).encoded
+    user = users(:quentin)
+    password = Password.new(:reset_code => 'abc', :user => user)
+    mailer = PasswordMailer.create_forgot_password(password)
+    
+    assert_equal "[#{APP_CONFIG[:site_name]}] You have requested to change your password", mailer.subject
+    assert mailer.from.include?(APP_CONFIG[:admin_email])
+    assert mailer.to.include?('quentin@example.com')
+    assert mailer.body =~ /#{APP_CONFIG[:site_url]}\/change_password\/abc/
   end
 
   def test_reset_password
-    @expected.subject = 'PasswordMailer#reset_password'
-    @expected.body    = read_fixture('reset_password')
-    @expected.date    = Time.now
+    user = users(:quentin)
+    mailer = PasswordMailer.create_reset_password(user)
 
-    assert_equal @expected.encoded, PasswordMailer.create_reset_password(@expected.date).encoded
+    assert_equal "[#{APP_CONFIG[:site_name]}] Your password has been reset.", mailer.subject
+    assert mailer.from.include?(APP_CONFIG[:admin_email])
+    assert mailer.to.include?('quentin@example.com')
   end
 
 end
