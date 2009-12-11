e0403dc2d7ec72697c9c3fe9b9c7e429fedacc75
Jaime Bellmyer <bellmyer@happy-laptop.local>
2009-12-11 14:19:34 -0600

    Fixed ImagesController tests
    Got rid of restful tests for non-existant actions.
    
    Ensured success for admins and members, and redirect to login for visitors.
    
    Updated fixture so that values create a valid image record

--- test/functional/images_controller_test.rb
+++ test/functional/images_controller_test.rb
@@ -1,45 +1,76 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionController::TestCase
-  def test_should_get_index
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:images)
+  include AuthenticatedTestHelper
+  
+  def setup
+    @valid = {:size => 1, :content_type => 'image/jpeg', :filename => 'test.jpg'}
   end
-
-  def test_should_get_new
+  
+  def test_as_admin_getting_new_should_succeed
+    login_as(:admin)
     get :new
     assert_response :success
   end
 
-  def test_should_create_image
+  def test_as_member_getting_new_should_succeed
+    login_as(:quentin)
+    get :new
+    assert_response :success
+  end
+
+  def test_as_visitor_getting_new_should_be_redirected_to_login
+    get :new
+    assert_redirected_to new_session_path
+  end
+
+  def test_as_admin_creating_images_should_succeed
+    login_as :admin
+    
     assert_difference('Image.count') do
-      post :create, :image => { }
+      post :create, :image => @valid
     end
 
-    assert_redirected_to image_path(assigns(:image))
-  end
-
-  def test_should_show_image
-    get :show, :id => images(:one).id
     assert_response :success
   end
 
-  def test_should_get_edit
-    get :edit, :id => images(:one).id
+  def test_as_member_creating_images_should_succeed
+    login_as :quentin
+    
+    assert_difference('Image.count') do
+      post :create, :image => @valid
+    end
+
     assert_response :success
   end
 
-  def test_should_update_image
-    put :update, :id => images(:one).id, :image => { }
-    assert_redirected_to image_path(assigns(:image))
+  def test_as_visitor_creating_images_should_be_redirected_to_login
+    post :create, :image => {}
+    assert_redirected_to new_session_path
   end
 
-  def test_should_destroy_image
+  def test_as_admin_destroying_image_should_succeed
+    login_as :admin
+    
     assert_difference('Image.count', -1) do
       delete :destroy, :id => images(:one).id
     end
 
-    assert_redirected_to images_path
+    assert_redirected_to new_image_path
+  end
+
+  def test_as_member_destroying_image_should_succeed
+    login_as :quentin
+    
+    assert_difference('Image.count', -1) do
+      delete :destroy, :id => images(:one).id
+    end
+
+    assert_redirected_to new_image_path
+  end
+
+  def test_as_visitor_destroying_image_should_be_redirected_to_login
+    delete :destroy, :id => 1
+    assert_redirected_to new_session_path
   end
 end
